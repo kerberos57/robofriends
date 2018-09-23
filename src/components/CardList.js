400300e4be910fd3c3148ecb89da9af18f35839a
ganaa <ganaa@users.noreply.github.com>
2018-09-23 20:26:00 +0200

    key.index changed

--- src/components/CardList.js
+++ src/components/CardList.js
@@ -4,7 +4,13 @@ import Card from './Card';
 const CardList = ({ robots }) => (
   <div>
     {robots.map((user, i) => (
-      <Card key={i} id={robots[i].id} name={robots[i].name} username={robots[i].username} email={robots[i].email} />
+      <Card
+        key={robots.i}
+        id={robots[i].id}
+        name={robots[i].name}
+        username={robots[i].username}
+        email={robots[i].email}
+      />
     ))}
   </div>
 );
