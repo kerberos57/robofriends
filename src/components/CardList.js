1ff6b5e1dda321d4d55d3259970c40dbaa7ef99b
ganaa <ganaa@users.noreply.github.com>
2018-09-23 17:05:43 +0200

    robofriends completed

---
+++ src/components/CardList.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import Card from './Card';
+
+const CardList = ({ robots }) => (
+  <div>
+    {robots.map((user, i) => (
+      <Card key={i} id={robots[i].id} name={robots[i].name} username={robots[i].username} email={robots[i].email} />
+    ))}
+  </div>
+);
+
+export default CardList;
